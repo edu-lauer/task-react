8a10c47a13681b62be61fa1c7b67f249eb6563b5
Eduardo <lauer.edu@gmail.com>
2023-06-25 12:55:34 -0300

    feature to add new task done

--- Task-Organizer/src/components/Table/Table.jsx
+++ Task-Organizer/src/components/Table/Table.jsx
@@ -1,9 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import './table.scss'
 import LineTable from "../LineTable/LineTable"
 import addImg from '../../assets/img/add.svg'
 
 const Table = ({ data }) => {
+
+    const addNewTask = () => {
+        if (!taskDesc) {
+            alert("Inserir nova tarefa...")
+        } else {
+            const newTask = {
+                "id": data.length + 1,
+                "title": taskDesc,
+                "description": "",
+                "completed": false  
+            }
+
+            data.push(newTask)
+            
+            setDesc("")
+            
+            console.log(data)
+        }
+    }
+
+
+
+    const [taskDesc, setDesc] = useState("")
+
     return (
         <section className="mainContainer">
             <h1 className="mainContainer__title">Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
@@ -15,13 +39,18 @@ const Table = ({ data }) => {
                 </tr>
                 {
                     data.map((task) => (
-                        <LineTable key={task.id} data={task}/>
+                        <LineTable key={task.id} data={task} />
                     ))
-                }  
+                }
                 <tr className="mainContainer__table-lastLine">
-                    <input className="mainContainer__table-lastLine-firstColumn" type="text" placeholder="Nova tarefa..."/>
-                    <img className="mainContainer__table-lastLine-lastColumn" src={addImg} alt=""></img>
-                </tr>                                                                               
+                    <input
+                        className="mainContainer__table-lastLine-firstColumn"
+                        type="text"
+                        value={taskDesc}
+                        onChange={(e) => setDesc(e.target.value)}
+                        placeholder="Nova tarefa..." />
+                    <img className="mainContainer__table-lastLine-lastColumn" src={addImg} alt="" onClick={addNewTask}></img>
+                </tr>
             </table>
         </section>
     )
