09f85338dedc83dc62c81f11dffb89dc18ae78cc
Eduardo <lauer.edu@gmail.com>
2023-06-25 11:02:36 -0300

    LineTable checkbox javascript logic

--- Task-Organizer/src/components/LineTable/LineTable.jsx
+++ Task-Organizer/src/components/LineTable/LineTable.jsx
@@ -1,14 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import './lineTable.scss'
 import editImg from '../../assets/img/edit.svg'
 import deleteImg from '../../assets/img/delete.svg'
-import addImg from '../../assets/img/add.svg'
 
 const LineTable = ({ data }) => {
+
+    const [check, setCheck] = useState()
+    data.completed = check
+
     return (
         <tr className="itemToDo">
             <th className="itemToDo__title">{data.title}</th>
-            <th className="itemToDo__status"><input type="checkbox"/></th>
+            <th className="itemToDo__status"><input type="checkbox" checked={data.completed} onClick={() => {
+                if(data.completed) {
+                    setCheck(false)
+                    console.log(data) // Não sei se esta correto dessa forma
+                } else {
+                    setCheck(true)
+                    console.log(data) // Não sei se esta correto dessa forma
+                }
+            }} /></th>
             <th className="itemToDo__options">
                 <img className="itemToDo__option-edit" src={editImg} alt="" />
                 <img className="itemToDo__option-delete" src={deleteImg} alt="" />
