bfa5c33e2d34e33bbd41d1ccf84db1f49ef8322c
Eduardo <lauer.edu@gmail.com>
2023-07-01 06:50:01 -0300

    added h1 that was previous at Table component to Header component

--- Task-Organizer/src/components/Header/Header.jsx
+++ Task-Organizer/src/components/Header/Header.jsx
@@ -3,12 +3,17 @@ import './header.scss'
 
 const Header = () => {
     return (
-        <div className="header">
-            <ul className="header__list">
-                <li className="header__list-item"><a className="header__list-item-link" href="">Organização</a></li>
-                <li className="header__list-item"><a className="header__list-item-link" href="">Tarefas</a></li>
-            </ul>
-        </div>
+        <>
+            <div className="header">
+                <ul className="header__list">
+                    <li className="header__list-item"><a className="header__list-item-link" href="">Organização</a></li>
+                    <li className="header__list-item"><a className="header__list-item-link" href="">Tarefas</a></li>
+                </ul>
+            </div>
+            <div>
+                <h1 className="title">Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
+            </div>
+        </>
     )
 }
 
