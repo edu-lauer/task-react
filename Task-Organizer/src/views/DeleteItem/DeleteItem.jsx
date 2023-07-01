b58fe8e668989c73d5c7b92f6924b6dd12859d40
Eduardo <lauer.edu@gmail.com>
2023-07-01 06:52:40 -0300

    created EditItem and DeleteItem as a view

---
+++ Task-Organizer/src/views/DeleteItem/DeleteItem.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from '../../components/Header/Header'
+
+const DeleteItem = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default DeleteItem
---
+++ Task-Organizer/src/views/EditItem/EditItem.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import './editItem.scss'
+import Header from '../../components/Header/Header'
+import EditModal from "../../components/EditModal/EditModal"
+
+const EditItem = () => {
+    return (
+        <>
+            <Header />
+            <EditModal />
+        </>
+    )
+}
+
+export default EditItem
