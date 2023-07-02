d103072230fbe484a1a7a7f09e0e3faee60235ea
Eduardo <lauer.edu@gmail.com>
2023-07-02 08:18:53 -0300

    features to get task title and edit

--- Task-Organizer/src/components/EditModal/EditModal.jsx
+++ Task-Organizer/src/components/EditModal/EditModal.jsx
@@ -1,15 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
+import { useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import './editModal.scss'
 
 
 
-const EditModal = () => {
+const EditModal = ({ data }) => {
+
+    const navigate = useNavigate()
+
+    const { taskId } = useParams()
+    const selectedTask = data.find((task) => task.id == taskId)
+
+    const [newTitle, setNewTitle] = useState(`${selectedTask.title}`)
+
+    const taskEdited = () => {
+        if (newTitle) {
+            selectedTask.title = newTitle
+            navigate('/')
+        } else {
+            alert('Titulo vazio')
+        }
+    }
+
     return (
         <>
             <div className="editModalContainerMain">
                 <div className="editModal">
                     <h1 className="editModal__title">Deseja editar esse item?</h1>
-                    <h5 className="editModal__subtitle">Colocar as descrições da tarefa aqui.</h5>
+                    <input
+                        className="editModal__subtitle"
+                        type="text"
+                        value={newTitle}
+                        onChange={(e) => setNewTitle(e.target.value)} />
                     <div className="editModal__btns">
                         <button className="editModal__btns-no">Não</button>
                         <button className="editModal__btns-yes">Sim</button>
